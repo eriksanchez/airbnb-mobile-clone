8b74730d0a9525337326733391302a642dec77f7
eriksanchez <erikcsanchezdev@gmail.com>
2020-08-07 13:59:04 -0500

    Explore page in the works...

--- App.js
+++ App.js
@@ -1,21 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { createBottomTabNavigator } from "react-navigation";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Icon from "react-native-vector-icons/Ionicons";
+import Explore from "./screens/Explore";
+import Saved from "./screens/Saved";
+import Inbox from "./screens/Inbox";
+import Trips from "./screens/Trips";
+import Profile from "./screens/Profile";
 
+export default createBottomTabNavigator(
+  {
+    Explore: {
+      screen: Explore,
+      navigationOptions: {
+        tabBarLabel: "EXPLORE",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-search" color={tintColor} size={24} />
+        ),
+      },
+    },
+    Saved: {
+      screen: Saved,
+      navigationOptions: {
+        tabBarLabel: "SAVED",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-heart" color={tintColor} size={24} />
+        ),
+      },
+    },
+    Trips: {
+      screen: Trips,
+      navigationOptions: {
+        tabBarLabel: "TRIPS",
+        tabBarIcon: ({ tintColor }) => (
+          <Image
+            source={require("./assets/airbnb.png")}
+            style={{ height: 24, width: 24, tintColor: tintColor }}
+          />
+        ),
+      },
+    },
+    Inbox: {
+      screen: Inbox,
+      navigationOptions: {
+        tabBarLabel: "INBOX",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-chatboxes" color={tintColor} size={24} />
+        ),
+      },
+    },
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        tabBarLabel: "PROFILE",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-person" color={tintColor} size={24} />
+        ),
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: "red",
+      inactiveTintColor: "grey",
+      style: {
+        backgroundColor: "white",
+        borderTopWidth: 0,
+        shadowOffset: { width: 5, height: 3 },
+        shadowColor: "black",
+        shadowOpacity: 0.5,
+        elevation: 5,
+      },
+    },
+  }
+);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
